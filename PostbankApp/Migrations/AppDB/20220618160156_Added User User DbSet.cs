fcc6f253745cd21b587d2a361813238b4c81393b
OKAY808 <georgedautov808@gmail.com>
2022-06-18 19:49:48 +0300

    reverted back some changes

---
+++ PostbankApp/Migrations/AppDB/20220618160156_Added User User DbSet.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostbankApp.Migrations.AppDB
+{
+    public partial class AddedUserUserDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CardNumber",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
