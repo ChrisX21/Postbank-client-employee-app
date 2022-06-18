f72cf840b6014aeae2fda3a659adbebd068f984b
ChrisX21 <chrisalexiev@icloud.com>
2022-06-18 22:48:17 +0300

    removed unnecessary files and some extensions

--- PostbankApp/Models/Sale.cs
+++ PostbankApp/Models/Sale.cs
@@ -2,8 +2,6 @@
 {
     public class Sale
     {
-        public string Id { get; set; }
-        public string SalerId { get; set; }
         public int SaleValue { get; set; }
         public string StartDate { get; set; }
         public string EndDate { get; set; }
