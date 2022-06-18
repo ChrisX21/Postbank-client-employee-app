1da413b8380bcf311071226a8cce2fa396122f18
MrLoveMilk <dani27game@gmail.com>
2022-06-18 16:28:18 +0300

    Fixed buttons

---
+++ PostbankApp/Areas/Identity/Pages/Account/CreateSale.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PostbankApp.Areas.Identity.Pages.Account
+{
+    public class CreateSaleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
